d0dd9519e08e350d2925762b2cd14f7320e2fe54
Rahmoune Wafaà <w.rahmoune@esi-sba.dz>
2025-10-10 08:08:07 +0100

    Update Forgotpassword.jsx

--- src/pages/user/Forgotpassword.jsx
+++ src/pages/user/Forgotpassword.jsx
@@ -55,7 +55,7 @@ export default function ForgotPassword() {
 
     setIsSending(true);
     try {
-      const res = await axios.post("http://localhost:8000/api/forgot-password/", {
+      const res = await axios.post("https://api.kadi-inv.store/api/forgot-password/", {
         email: formData.email,
       });
 
